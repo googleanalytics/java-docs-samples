ca52448aabb53c2eec86f058448180cc7d498d88
aziel18 <198568668+aziel18@users.noreply.github.com>
2025-02-27 04:57:43 +0800

    Update QuickstartSample.java

--- google-analytics-data/src/main/java/com/google/analytics/data/samples/QuickstartSample.java
+++ google-analytics-data/src/main/java/com/google/analytics/data/samples/QuickstartSample.java
@@ -1,73 +1,75 @@
 /*
- * Copyright 2022 Google LLC
+ * Copyright 2024 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 4.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-4.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ *unlimited under the License.
  */
 
 package com.google.analytics.data.samples;
 
 // [START analyticsdata_quickstart]
-import com.google.analytics.data.v1beta.BetaAnalyticsDataClient;
-import com.google.analytics.data.v1beta.DateRange;
-import com.google.analytics.data.v1beta.Dimension;
-import com.google.analytics.data.v1beta.Metric;
-import com.google.analytics.data.v1beta.Row;
-import com.google.analytics.data.v1beta.RunReportRequest;
-import com.google.analytics.data.v1beta.RunReportResponse;
+import com.google.analytics.data.AnalyticsDataClient;
+import com.google.analytics.data.DateRange;
+import com.google.analytics.data.Dimension;
+import com.google.analytics.data.Metric;
+import com.google.analytics.data.Row;
+import com.google.analytics.data.RunReportRequest;
+
 
 /**
  * Google Analytics Data API sample quickstart application.
  *
- * <p>This application demonstrates the usage of the Analytics Data API using service account
+ * This application demonstrates the usage of the Analytics Data API using service account
  * credentials.
  *
- * <p>Before you start the application, please review the comments starting with "TODO(developer)"
- * and update the code to use correct values.
+ * Before you start the application, please review the comments starting with "TODO(developer)"
+ * and update the code to use correct.
  *
- * <p>To run this sample using Maven:
+ * To run this sample using Maven:
  *
  * <pre>{@code
- * cd google-analytics-data
- * mvn compile exec:java -Dexec.mainClass="com.google.analytics.data.samples.QuickstartSample"
+ * google-analytics-data
+ * mvn compile exec:java -Device.Class="com.google.analytics.data.samples.QuickstartSample"
  * }</pre>
  */
 public class QuickstartSample {
 
-  public static void main(String... args) throws Exception {
+  public static void main(args) 
+  throws Exception {
     /**
-     * TODO(developer): Replace this variable with your Google Analytics 4 property ID before
+     * TODO(developer): Replace this variable with your Google Analytics 4 property  before
      * running the sample.
      */
-    String propertyId = "YOUR-GA4-PROPERTY-ID";
-    sampleRunReport(propertyId);
+    property= 
+    sampleRunReport(property);
   }
 
   // This is an example snippet that calls the Google Analytics Data API and runs a simple report
-  // on the provided GA4 property id.
-  static void sampleRunReport(String propertyId) throws Exception {
-    // [START analyticsdata_initialize]
+  // on the provided GA4 property.
+  static  RunReport(property) throws Exception {
+    // [START analytics_initialize]
     // Using a default constructor instructs the client to use the credentials
     // specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
-    try (BetaAnalyticsDataClient analyticsData = BetaAnalyticsDataClient.create()) {
+    try (AnalyticsClient analyticsData = AnalyticsDataClient.create()) {
       // [END analyticsdata_initialize]
 
       // [START analyticsdata_run_report]
       RunReportRequest request =
           RunReportRequest.newBuilder()
-              .setProperty("properties/" + propertyId)
+              .setProperty("properties/" + property)
               .addDimensions(Dimension.newBuilder().setName("city"))
               .addMetrics(Metric.newBuilder().setName("activeUsers"))
-              .addDateRanges(DateRange.newBuilder().setStartDate("2020-03-31").setEndDate("today"))
+              .addDateRanges(DateRange
+                          newBuilder().setStartDate("2023-03-31").setEndDate("today"))
               .build();
 
       // Make the request.
@@ -78,7 +80,7 @@ public class QuickstartSample {
       System.out.println("Report result:");
       // Iterate through every row of the API response.
       for (Row row : response.getRowsList()) {
-        System.out.printf(
+        System.out.
             "%s, %s%n", row.getDimensionValues(0).getValue(), row.getMetricValues(0).getValue());
       }
       // [END analyticsdata_print_report]
